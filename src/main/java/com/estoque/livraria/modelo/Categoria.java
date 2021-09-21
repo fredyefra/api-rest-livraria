b1b6f719d6aa9a8e9d9453e4553af650862da248
fredye <fredweb3@gmail.com>
2021-09-20 23:34:07 -0300

    classes de dominio adicionadas

--- src/main/java/com/estoque/livraria/modelo/Categoria.java
+++ src/main/java/com/estoque/livraria/modelo/Categoria.java
@@ -1,5 +1,84 @@
 package com.estoque.livraria.modelo;
 
-public class Categoria {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Categoria implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+    private Integer identificador;
+	private String nome;
+	private String descricao;
+    
+	private List<Livro> livros = new ArrayList<Livro>();
+	
+	public Categoria() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Categoria(Integer identificador, String nome, String descricao) {
+		super();
+		this.identificador = identificador;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Integer getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(Integer identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((identificador == null) ? 0 : identificador.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (identificador == null) {
+			if (other.identificador != null)
+				return false;
+		} else if (!identificador.equals(other.identificador))
+			return false;
+		return true;
+	}
 }
--- src/main/java/com/estoque/livraria/modelo/Livro.java
+++ src/main/java/com/estoque/livraria/modelo/Livro.java
@@ -1,5 +1,75 @@
 package com.estoque.livraria.modelo;
 
-public class Livro {
+import java.io.Serializable;
+
+public class Livro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+    private Integer identificador;
+    private String nomeAutor;
+    private String texto;
+    private Categoria categoria;
+    
+	public Livro() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Livro(Integer identificador, String nomeAutor, String texto) {
+		super();
+		this.identificador = identificador;
+		this.nomeAutor = nomeAutor;
+		this.texto = texto;
+	}
+
+
+	public Integer getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(Integer identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((identificador == null) ? 0 : identificador.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (identificador == null) {
+			if (other.identificador != null)
+				return false;
+		} else if (!identificador.equals(other.identificador))
+			return false;
+		return true;
+	}
 
 }
